f122c59aafa0462487ed9999b21e76d090504261
Galex Yen <galexy@users.noreply.github.com>
2017-10-28 18:36:26 -0700

    Updates

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# bash
+ln -s ${BASEDIR}/bash/bash_profile ~/.bash_profile
+ln -s ${BASEDIR}/bash/bashrc ~/.bashrc
+
+# spacemacs
+ln -s ${BASEDIR}/emacs/spacemacs ~/.emacs.d/spacemacs
+
+
