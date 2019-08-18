42e8f42a5856aa975132085f8721756867e0bef6
Galex Yen <galexy@users.noreply.github.com>
2019-08-17 18:29:32 -0700

    Adding gitconfig

--- install.sh
+++ install.sh
@@ -3,11 +3,20 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # bash
-ln -s ${BASEDIR}/bash/bash_profile ~/.bash_profile
-ln -s ${BASEDIR}/bash/bashrc ~/.bashrc
+echo "Linking bash config"
+ln -f -s ${BASEDIR}/bash/bash_profile ~/.bash_profile
+ln -f -s ${BASEDIR}/bash/bashrc ~/.bashrc
 
 # spacemacs
-ln -s ${BASEDIR}/emacs/spacemacs ~/.spacemacs
+echo "Linking spacemacs config"
+ln -f -s ${BASEDIR}/emacs/spacemacs ~/.spacemacs
 
+# powerline
+echo "Linking powerline config"
 mkdir -p ~/.config/powerline
-ln -s ${BASEDIR}/powerline/config.json ~/.config/powerline/config.json
+ln -f -s ${BASEDIR}/powerline/config.json ~/.config/powerline/config.json
+
+# git
+echo "LInking git config"
+ln -f -s ${BASEDIR}/git/gitconfig ~/.gitconfig
+
