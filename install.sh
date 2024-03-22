d2e471d64794213bbffcdeb7239fdd72cf1b57dc
Galex Yen <616667+galexy@users.noreply.github.com>
2024-03-22 08:42:25 -0700

    Add option to choose which config to install

--- install.sh
+++ install.sh
@@ -1,70 +1,110 @@
 #!/bin/bash
 
+# Get an optional configuration to install
+if [ -z "$1" ]; then
+    echo "No configuration specified, installing all"
+    CONFIG="all"
+else
+    CONFIG="$1"
+fi
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # bash
-echo "Linking bash config"
-ln -f -s ${BASEDIR}/bash/bash_profile ~/.bash_profile
-ln -f -s ${BASEDIR}/bash/bashrc ~/.bashrc
-ln -f -s ${BASEDIR}/bash/bash_aliases ~/.bash_aliases
-ln -f -s ${BASEDIR}/bash/profile ~/.profile
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "bash" ]; then
+    echo "Linking bash config"
+    ln -f -s ${BASEDIR}/bash/bash_profile ~/.bash_profile
+    ln -f -s ${BASEDIR}/bash/bashrc ~/.bashrc
+    ln -f -s ${BASEDIR}/bash/bash_aliases ~/.bash_aliases
+    ln -f -s ${BASEDIR}/bash/profile ~/.profile
+fi
 
-echo "Linking zsh config"
-mkdir -p ~/.zsh
-ln -f -s ${BASEDIR}/zsh/completion ~/.zsh/completion
-ln -f -s ${BASEDIR}/zsh/zshrc ~/.zshrc
-ln -f -s ${BASEIDR}/zsh/zshenv ~/.zshenv
-ln -f -s ${BASEDIR}/zsh/zprofile ~/.zprofile
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "zsh" ]; then
+    echo "Linking zsh config"
+    mkdir -p ~/.zsh
+    ln -f -s ${BASEDIR}/zsh/completion ~/.zsh/completion
+    ln -f -s ${BASEDIR}/zsh/zshrc ~/.zshrc
+    ln -f -s ${BASEIDR}/zsh/zshenv ~/.zshenv
+    ln -f -s ${BASEDIR}/zsh/zprofile ~/.zprofile
+fi
 
-# spacemacs
-echo "Linking spacemacs config"
-ln -f -s ${BASEDIR}/emacs/spacemacs ~/.spacemacs
-echo "Linking custom private layer"
-ln -f -s ${BASEDIR}/emacs/galexy ~/.emacs.d/private/galexy
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "spacemacs" ]; then
+    # spacemacs
+    echo "Linking spacemacs config"
+    ln -f -s ${BASEDIR}/emacs/spacemacs ~/.spacemacs
+    echo "Linking custom private layer"
+    ln -f -s ${BASEDIR}/emacs/galexy ~/.emacs.d/private/galexy
+fi
 
 # powerline
-echo "Linking powerline config"
-mkdir -p ~/.config/powerline
-ln -f -s ${BASEDIR}/powerline/config.json ~/.config/powerline/config.json
-ln -f -s ${BASEDIR}/powerline/themes ~/.config/powerline/themes
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "powerline" ]; then
+    echo "Linking powerline config"
+    mkdir -p ~/.config/powerline
+    ln -f -s ${BASEDIR}/powerline/config.json ~/.config/powerline/config.json
+    ln -f -s ${BASEDIR}/powerline/themes ~/.config/powerline/themes
+fi
 
 # git
-echo "Linking git config"
-ln -f -s ${BASEDIR}/git/gitconfig ~/.gitconfig
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "git" ]; then
+    echo "Linking git config"
+    ln -f -s ${BASEDIR}/git/gitconfig ~/.gitconfig
+fi
 
 # tmux
-echo "Linking tmux config"
-ln -f -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "tmux" ]; then
+    echo "Linking tmux config"
+    ln -f -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+fi
 
 # local bin
-echo "Linking local bin files"
-ln -f -s ${BASEDIR}/bin/* ~/.local/bin
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "bin" ]; then
+    echo "Linking local bin files"
+    ln -f -s ${BASEDIR}/bin/* ~/.local/bin
+fi
 
 # ghci
-echo "Linking GHCi configuration"
-mkdir -p ~/.ghc/
-ln -f -s ${BASEDIR}/ghc/ghci.conf ~/.ghc/ghci.conf
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "ghci" ]; then
+    echo "Linking GHCi configuration"
+    mkdir -p ~/.ghc/
+    ln -f -s ${BASEDIR}/ghc/ghci.conf ~/.ghc/ghci.conf
+fi
 
 # readline
-echo "Linking readline inputrc"
-ln -f -s ${BASEDIR}/readline/inputrc ~/.inputrc
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "readline" ]; then
+    echo "Linking readline inputrc"
+    ln -f -s ${BASEDIR}/readline/inputrc ~/.inputrc
+fi
 
-echo "Linking Cargo configuration"
-mkdir -p ~/.cargo
-ln -f -s ${BASEDIR}/cargo/env ~/.cargo/env
+# cargo
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "cargo" ]; then
+    echo "Linking Cargo configuration"
+    mkdir -p ~/.cargo
+    ln -f -s ${BASEDIR}/cargo/env ~/.cargo/env
+fi
 
-echo "Linking Guile Configuration"
-ln -f -s ${BASEDIR}/guile/guile ~/.guile
+# guile
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "guile" ]; then
+    echo "Linking Guile Configuration"
+    ln -f -s ${BASEDIR}/guile/guile ~/.guile
+fi
 
-echo "Linking AWS CLI Configuration"
-mkdir -p ~/.aws
-ln -f -s ${BASEDIR}/aws/config ~/.aws/config
+# AWS cli
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "aws" ]; then
+    echo "Linking AWS CLI Configuration"
+    mkdir -p ~/.aws
+    ln -f -s ${BASEDIR}/aws/config ~/.aws/config
+fi
 
-echo "Linking Cabal Configuration"
-mkdir -p ~/.cabal
-ln -f -s ${BASEDIR}/cabal/config ~/.cabal/config
+# cabal
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "cabal" ]; then
+    echo "Linking Cabal Configuration"
+    mkdir -p ~/.cabal
+    ln -f -s ${BASEDIR}/cabal/config ~/.cabal/config
+fi
 
-echo "Linking 1Password SSH Configuration"
-mkdir -p ~/.config/1Password/ssh
-ln -f -s ${BASEDIR}/1Password/ssh/agent.toml ~/.config/1Password/ssh/agent.toml
-
+# 1Password
+if [ "$CONFIG" == "all" ] || [ "$CONFIG" == "1password" ]; then
+    echo "Linking 1Password Configuration"
+    mkdir -p ~/.config/1Password
+    ln -f -s ${BASEDIR}/1Password/config.toml ~/.config/1Password/config.toml
+fi
